9ec33abcb2f26b48700e86bc738e59c841742a04
Salem <spencernavarro2004@gmail.com>
2024-06-03 13:32:10 -0700

    🐛 RegularButton interface now only requires `Style` property

--- src/Interfaces/Discord/RegularButton.ts
+++ src/Interfaces/Discord/RegularButton.ts
@@ -3,7 +3,7 @@ import type { BaseComponent } from "./BaseComponent";
 
 export interface RegularButton extends BaseComponent {
     style: Exclude<ButtonStyle, ButtonStyle.Link>;
-    disabled: boolean;
-    emoji: APIMessageComponentEmoji;
-    label: string;
+    disabled?: boolean;
+    emoji?: APIMessageComponentEmoji;
+    label?: string;
 };
